af00e11c16787ecdc16e703d677bc74874a40065
Victor Oderinde <>
2022-10-22 06:46:10 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let rangeMax = 2;
+let userName = prompt("what is your name?")
+
+function guesser(range=2) {
+    let randomNum = Math.floor(Math.random() * range) + 1;
+  
+  while(level < 10) {
+    
+  }
+}
+
+console.log("ade")
